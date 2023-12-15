156f584c71981800d9da9fda294554b487f7428c
Abhijith Nellikkat <abhijithnellikkat@gmail.com>
2023-12-15 15:34:41 +0530

    MethodChannel sample toast message completed

--- android/app/src/main/kotlin/com/example/platform_channels_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/platform_channels_app/MainActivity.kt
@@ -1,6 +1,29 @@
 package com.example.platform_channels_app
 
+
+import android.widget.Toast
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
 
 class MainActivity: FlutterActivity() {
+
+
+    private  val channelName = "toastChannel";
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        var channel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger , channelName);
+
+        channel.setMethodCallHandler { call, result ->
+            if(call.method == "showToast"){
+                Toast.makeText(this, "Method Channel Sample Toast !!! ", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List ringtones = [];
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +23,38 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  var channel = const MethodChannel('toastChannel');
+
+  showToast() {
+    channel.invokeMethod("showToast");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Platform channel'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: OutlinedButton(
+          onPressed: showToast,
+          child: const Text('Show toast'),
+        ),
       ),
     );
   }
